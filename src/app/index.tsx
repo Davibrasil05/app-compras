fd0e7c3b2acb6c1b2f1f80ab809ff93b256f94c0
Davibrasil05 <davi.brasil2005@gmail.com>
2025-11-06 17:43:00 -0400

    Aprendendo react native

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from "react-native"
+import { styles } from "../styles"
+export default function App(){
+    return(
+        <View style={styles.container}>
+            <Text style={styles.text}>
+                Teste1
+            </Text>
+        </View>
+    )
+}
+
---
+++ src/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+
+export const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center",
+        backgroundColor: "#72dde7ff",
+    },
+    text:{
+        color: "#151617ff",
+        fontSize: 43,
+        fontWeight: "bold"
+    }
+})
+
