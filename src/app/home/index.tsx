9210537ce8cd8338bd2a7765db54884ba4a99221
Davibrasil05 <davi.brasil2005@gmail.com>
2025-11-07 23:51:51 -0400

    Adicionando componente filter

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,17 +1,30 @@
 import { Button } from "@/components/Button"
 import { Input } from "@/components/Input"
 import { Image, View } from "react-native"
-import { styles } from "./styles"
 
+import { Filter } from "@/components/Filter"
+import { FilterStatus } from "@/types/FilterStatus"
+import { styles } from "./styles"
 
 export default function App(){
     return(
         <View style={styles.container}>
             
             <Image source={require("@/assets/logo.png")} style={styles.logo} />
-            <Input placeholder="O que você precisa comprar? "/>
-            <Button title ="Entrar"/>
+
+            <View style={styles.form}>
+                <Input placeholder="O que você precisa comprar? "/>
+                <Button title ="Entrar"/>
+                <Button title ="teste"/>
+            </View>
+
+            <View style={styles.content}>
+                <Filter status={FilterStatus.DONE} isActive /> 
+                <Filter status={FilterStatus.PENDING} isActive={false}/>
+            </View> 
         </View>
+
+
     )
 }
 
---
+++ src/components/Filter/index.tsx
@@ -0,0 +1,19 @@
+import { Text, TouchableOpacity, TouchableOpacityProps } from "react-native";
+
+import { FilterStatus } from "@/types/FilterStatus";
+import { styles } from "./styles";
+
+type Props = TouchableOpacityProps &{
+    status: FilterStatus,
+    isActive: boolean,
+}
+
+export function Filter({status, isActive, ...rest}:Props){
+    return(
+        <TouchableOpacity style={[styles.container, {opacity: isActive ? 1 :0.5}]} 
+        {...rest}
+        >
+            <Text style={styles.title}>{status === FilterStatus.DONE ? "Comprados" : "Pendentes"}</Text>
+        </TouchableOpacity>
+    )
+}
