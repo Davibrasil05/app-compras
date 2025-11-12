992b08e3d51fa312b9d7ce5795d5699e4436ac08
Davibrasil05 <davi.brasil2005@gmail.com>
2025-11-12 00:17:21 -0400

    Adicionando o método remove e atualizando add

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -11,46 +11,63 @@ import { styles } from "./styles"
 
 
 export default function App(){
-    const [filter, setFilter] = useState(FilterStatus.PENDING)
-    const [description, setDescription] = useState("")
-    const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE]
-    const [items,setItems] = useState<ItemStorage[]>([])
+    const [filter, setFilter] = useState(FilterStatus.PENDING) //use state de filtro e description
+    const [description, setDescription] = useState("") 
+    const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE] //array de estados, ou pendente ou feito
+    const [items,setItems] = useState<ItemStorage[]>([]) //useState de cada item, do tipo de ItemStorage[]
     
-    async function handleAdd(){
-        if(!description.trim()){
-            return Alert.alert("Adicionar", "Informe a descrição para adicionar")
+    async function handleAdd(){ //função assincrona para adicionar na lista
+        if(!description.trim()){ //trim é para retirar os espaços da string
+            return Alert.alert("Adicionar", "Informe a descrição para adicionar") //Alerta caso não tenha nada no aplicativo
         }
         
 
-        const newItem = {
+        const newItem = { //variável para o item
             id: Math.random().toString(36).substring(2),
             description,
             status: FilterStatus.PENDING,
 
         }
         
-        await itemsStorage.add(newItem)
-        await getItems()
+        await itemsStorage.add(newItem) //adiciona novo item a partir da função item de itemsStorage
+
+        Alert.alert("Adicionado",`Adicionado ${description}`)
+        setDescription("")
+        setFilter(FilterStatus.PENDING)
     }
-    async function getItems(){
+
+    async function itemsByStatus(){
         try{   
-            const response = await itemsStorage.get()
-            setItems(response)
+            const response = await itemsStorage.getByStatus(filter) //recebe a lista pelo método get e utilizar filter como parâmetroo
+            setItems(response) 
         } catch(error){
             Alert.alert("Erro", "Não foi possível filtrar os itens")
         }
     }
     
+    async function handleRemove(id: string){
+        try{
+            await itemsStorage.remove(id)
+            await itemsByStatus()
+        } catch(error){
+            console.log(error)
+            Alert.alert("Remover", "Não foi possível remover o item")
+        }
+
+
+    }
     useEffect(() => {
-        getItems()
-    }, [])
+        itemsByStatus()
+    }, [filter])
     return(
-        <View style={styles.container}>
-            
+        <View style={styles.container}>             
             <Image source={require("@/assets/logo.png")} style={styles.logo} />
 
             <View style={styles.form}>
-                <Input placeholder="O que você precisa comprar? " onChangeText={setDescription}/>
+                <Input placeholder="O que você precisa comprar? "
+                onChangeText={setDescription}
+                value={description}
+                />
                 <Button title ="Adicionar" onPress={handleAdd}/>
             </View>
 
@@ -77,7 +94,7 @@ export default function App(){
                         <Item 
                         data={item}
                         onStatus={() => console.log("Mudar o Status")} 
-                        onRemove={() => console.log("Remover")}></Item>
+                        onRemove={() => handleRemove(item.id)}></Item>
 
                     )}
                     showsVerticalScrollIndicator={false}
--- src/storage/itemsStorage.ts
+++ src/storage/itemsStorage.ts
@@ -1,9 +1,9 @@
 import { FilterStatus } from "@/types/FilterStatus"
 import AsyncStorage from "@react-native-async-storage/async-storage"
 
-const ITEMS_STORAGE_KEY = "@comprar:items"
+const ITEMS_STORAGE_KEY = "@comprar:items" //chave
 
-export type ItemStorage = {
+export type ItemStorage = { //tipo de cada item da lista
     id: string
     status: FilterStatus
     description: string
@@ -12,7 +12,7 @@ export type ItemStorage = {
 
 async function get(): Promise<ItemStorage[]> {
     try{
-        const storage = await AsyncStorage.getItem(ITEMS_STORAGE_KEY)
+        const storage = await AsyncStorage.getItem(ITEMS_STORAGE_KEY) //
         
         return storage ? JSON.parse(storage) : []
     } catch(error){
@@ -36,12 +36,19 @@ async function save(items: ItemStorage[]): Promise<void>{ //Função interna par
 
 async function add(newItem: ItemStorage): Promise<ItemStorage[]>{ //função de adicionar na lista, parâmetro é o novo item, retorna a lista total
     const items = await get() //promise confirma que o retorno vai ser um ItemStorage[]
-    const updatedItems = [...items, newItem]
-    await save(updatedItems)
+    const updatedItems = [...items, newItem] //variável que retorna toda  a lista e também o mais recente
+    await save(updatedItems) //
     return updatedItems
 }   
+async function remove(id: string){
+        const items = await get()
+        const updatedItems = items.filter((item) => item.id !== id)
+        await save(updatedItems)
+
+    }
 export const itemsStorage = {
     get,
     getByStatus,
     add,
+    remove,
 }
